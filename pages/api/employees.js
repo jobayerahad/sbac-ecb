288ead6cdb41e3c6c1c788c74bf435facf095f9c
Md Jobayer Al Mahmud Ahad <30753487+jobayerahad@users.noreply.github.com>
2022-05-17 19:23:23 +0600

    sorting fixes

--- pages/api/employees.js
+++ pages/api/employees.js
@@ -4,7 +4,7 @@ import Employee from 'models/Employee'
 const employees = async (_, res) => {
   try {
     connect()
-    const employees = await Employee.find().sort({ rank: 1 })
+    const employees = await Employee.find().sort({ rank: 1, emp_id: 1 })
     res.json(employees)
   } catch (e) {
     res.json({ error: e.message })
