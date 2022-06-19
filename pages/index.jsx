b31504aa0706fdd6e3386d29ad646ca131d938b4
Md Jobayer Al Mahmud Ahad <30753487+jobayerahad@users.noreply.github.com>
2022-06-19 17:20:50 +0600

    moved backend to another project

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,10 @@ import { RingLoader } from 'react-spinners'
 import { useQuery } from 'react-query'
 import Table from '@components/Table'
 
+const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/employees`
+
 const Home = () => {
-  const { isLoading, error, data } = useQuery('employeeData', async () => (await axios.get('/api/employees')).data)
+  const { isLoading, error, data } = useQuery('employeeData', async () => (await axios.get(apiUrl)).data)
 
   if (isLoading)
     return (
@@ -25,7 +27,7 @@ const Home = () => {
               {error.code === 'ECONNABORTED' ? 'Connection timeout' : 'Something went wrong'}
             </p>
 
-            <p className='error-description'>{error.message}</p>
+            <p className="error-description">{error.message}</p>
           </div>
         </div>
       </div>
